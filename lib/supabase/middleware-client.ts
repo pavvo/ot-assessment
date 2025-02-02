386a31637f7d1fc23bf00624aa91f68ffb2a14b3
odma-developer <developer@odma.delivery>
2025-02-02 17:04:59 +0100

    feat: add database typing

--- lib/supabase/middleware-client.ts
+++ lib/supabase/middleware-client.ts
@@ -2,12 +2,14 @@ import { type NextRequest, NextResponse } from 'next/server';
 
 import { createServerClient } from '@supabase/ssr';
 
+import { Database } from '~/database.types';
+
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   });
 
-  const supabase = createServerClient(
+  const supabase = createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
